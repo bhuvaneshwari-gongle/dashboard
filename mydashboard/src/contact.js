9fd22a1cdcdd8114e24b32daf18be4de31b08287
bhuvaneshwari-gongle <bhuvaneshwari.gongle@gmail.com>
2024-06-23 01:38:25 -0400

    icon update

--- mydashboard/src/contact.js
+++ mydashboard/src/contact.js
@@ -7,13 +7,13 @@ const Contact = () => {
             <h2>Contact</h2>
             <div className="contact-icons">
             <a href="https://www.linkedin.com/in/bgongle/" target="_blank" rel="noopener noreferrer">
-            <img src="/linkedin.png" alt="LinkedIn" className="icon linkedin-img"/>
+            <img src={`${process.env.PUBLIC_URL}/linkedin.png`} alt="LinkedIn" className="icon linkedin-img"/>
             </a>
             <a href="https://github.com/bhuvaneshwari-gongle" target="_blank" rel="noopener noreferrer">
-            <img src="/github.png" alt="GitHub" className="icon github-img"/>
+            <img src={`${process.env.PUBLIC_URL}/github.png`} alt="GitHub" className="icon github-img"/>
             </a>
             <a href="mailto:bhuvaneshwari.gongle@gmail.com">
-            <img src="/gmail.png" alt="Gmail" className="icon gmail-img"/>
+            <img src={`${process.env.PUBLIC_URL}/gmail.png`} alt="Gmail" className="icon gmail-img"/>
             </a>
             </div>
         </section>
