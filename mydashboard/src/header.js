4c48f91174e3f7cd0def2b515dc48306b965c090
bhuvaneshwari-gongle <bhuvaneshwari.gongle@gmail.com>
2024-06-22 21:40:39 -0400

    responsive commit

--- mydashboard/src/header.js
+++ mydashboard/src/header.js
@@ -1,17 +1,27 @@
-
-import React from 'react';
+// src/header.js
+import React, { useState } from 'react';
+import './header.css';
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <header>
             <h1>Bhuvaneshwari Gongle</h1>
-            <nav>
+            <nav className={`nav ${isOpen ? 'open' : ''}`}>
                 <ul>
                     <li><a href="#about">About</a></li>
                     <li><a href="#projects">Projects</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
             </nav>
+            <button className="hamburger" onClick={toggleMenu}>
+                &#9776; {/* Unicode for hamburger icon */}
+            </button>
         </header>
     );
 };
